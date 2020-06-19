3ab890b6e5d590f0004019f5ad5421ce4a53c51e
confucianzuoyuan <zuoyuantc@126.com>
2020-06-19 10:29:48 +0800

    day8

--- FlinkSZ1128/src/main/scala/com/atguigu/day7/FlinkTableExample.scala
+++ FlinkSZ1128/src/main/scala/com/atguigu/day7/FlinkTableExample.scala
@@ -56,11 +56,24 @@ object FlinkTableExample {
 
     val stream = env.addSource(new SensorSource)
 
+    // 将流转换成了动态表
     val table = tableEnv.fromDataStream(stream, 'id, 'timestamp as 'ts, 'temperature as 'temp)
     table
-      .select('id, 'temp)
-      .toAppendStream[(String, Double)]
+      .select('id, 'temp) // 生成了另一张动态表
+      // 将表的每一行转换成`(String, Double)`元组类型
+      .toAppendStream[(String, Double)] // 将动态表转换成了流
       .print()
+
+    // 时间属性
+    // 定义处理时间
+    val tableProcessingTime = tableEnv
+        // `'pt.proctime`：添加处理时间，字段名是`'pt`
+        .fromDataStream(stream, 'id, 'timestamp as 'ts, 'temperature as 'temp, 'pt.proctime)
+
+    // 定义事件时间
+    val tableEventTime = tableEnv
+        // 将timestamp指定为事件时间，并命名为ts
+        .fromDataStream(stream, 'id, 'timestamp.rowtime as 'ts, 'temperature as 'temp)
 
     env.execute()
   }
